cb5ed37c90b6725cb0c0eaff1db47a4a190ab696
sandovaljoaquin <joaquinsandoval74@yahoo.com>
2025-08-05 12:32:22 -0700

    C

--- Deritive_practice.R
+++ Deritive_practice.R
@@ -56,9 +56,26 @@ y <- seq(from = -0.4, to = 2.4, by = 0.1)
 
 eval(dt_dy)
 
+# small change, pushing/pulling github practice 
+
+# plotting example 
 
 
-# one small change, 
+library(ggplot2)
+library(palmerpenguins)
+
+ggplot(data = penguins, aes(x = body_mass_g, y = flipper_length_mm))+ 
+  geom_point(aes(color = species)) + 
+  scale_color_manual(values = c("darkorange", "purple", "cyan"))+ 
+  labs(x = "Body mass (g)", 
+       y = "Flipper length (mm)", 
+       title = "Palmer Penguins size measurements", 
+       subtitle = "Palmer archipelago, Antarctica (2007-2009)", 
+       caption = "Collected by Dr. Kristen Gorman") + 
+  facet_wrap(~island) + 
+  theme_minimal()
+
+
 
 
 
